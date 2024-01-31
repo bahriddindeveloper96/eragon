306e53058f3b104dd77e48f40b876eb06c3575ea
bahriddin <bahriddin.developer96@gmail.com>
2024-01-31 06:33:39 +0500

    (attribute)

---
+++ common/models/Company.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "company".
+ *
+ * @property int $id
+ * @property string $name
+ * @property int|null $inn
+ * @property int|null $phone
+ * @property string|null $address
+ * @property int $created_by
+ * @property int $updated_by
+ * @property int $created_at
+ * @property int $updated_at
+ *
+ * @property Vendor $createdBy
+ * @property Product[] $products
+ * @property Vendor $updatedBy
+ */
+class Company extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'company';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'created_by', 'updated_by', 'created_at', 'updated_at'], 'required'],
+            [['inn', 'phone', 'created_by', 'updated_by', 'created_at', 'updated_at'], 'integer'],
+            [['name', 'address'], 'string', 'max' => 255],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => Vendor::class, 'targetAttribute' => ['created_by' => 'id']],
+            [['updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => Vendor::class, 'targetAttribute' => ['updated_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'inn' => 'Inn',
+            'phone' => 'Phone',
+            'address' => 'Address',
+            'created_by' => 'Created By',
+            'updated_by' => 'Updated By',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|yii\db\ActiveQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(Vendor::class, ['id' => 'created_by']);
+    }
+
+    /**
+     * Gets query for [[Products]].
+     *
+     * @return \yii\db\ActiveQuery|ProductQuery
+     */
+    public function getProducts()
+    {
+        return $this->hasMany(Product::class, ['company_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[UpdatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|yii\db\ActiveQuery
+     */
+    public function getUpdatedBy()
+    {
+        return $this->hasOne(Vendor::class, ['id' => 'updated_by']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return CompanyQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new CompanyQuery(get_called_class());
+    }
+}
---
+++ console/migrations/m240131_011152_create_stock_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%stock}}`.
+ */
+class m240131_011152_create_stock_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%stock}}', [
+            'id' => $this->primaryKey(),
+            'product_id' => $this->integer(),
+            'attributes' => $this->json(),
+            'quantity' => $this->integer(),
+        ]);
+        $this->createIndex('index-stock_product_id', 'stock', 'product_id');
+        $this->addForeignKey('fkey-stock_product_id', 'stock', 'product_id', 'product', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%stock}}');
+    }
+}
