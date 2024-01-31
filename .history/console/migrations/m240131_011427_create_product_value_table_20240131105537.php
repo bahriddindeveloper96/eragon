15830c61dae113382d5da293c2c90f9ff16c6158
Bahriddin <bahriddin.developer962gmail.com>
2024-01-31 11:00:14 +0500

    (photo id to id)

--- .history/console/migrations/m240131_011427_create_product_value_table_20240131105537.php
+++ .history/console/migrations/m240131_011427_create_product_value_table_20240131105537.php
@@ -25,7 +25,7 @@ class m240131_011427_create_product_value_table extends Migration
         $this->createIndex('index-product_value_photo_id', 'product', 'photo_id');
         $this->addForeignKey('fkey-product_value_attribute_id', 'product_value', 'attribute_id', 'product_attribute', 'id', 'CASCADE', 'CASCADE');        
         $this->addForeignKey('fkey-product_value_product_id', 'product_value', 'product_id', 'product', 'id', 'CASCADE', 'CASCADE');
-        $this->addForeignKey('fkey-product_value_photo_id', 'product_value', 'photo_id', 'photo', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('fkey-product_value_photo_id', 'product', 'photo_id', 'photo', 'id', 'CASCADE', 'CASCADE');
     }
 
     /**
