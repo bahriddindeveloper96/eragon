70ceda073ec2a2967dc8beaa089705cd14794278
Bahriddin <bahriddin.developer962gmail.com>
2024-02-07 11:08:22 +0500

    (upgrade project)

---
+++ .history/backend/controllers/CategoryAttributeController_20240207105822.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace backend\controllers;
+
+use common\models\CategoryAttribute;
+use common\models\CategoryAttributeSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * ProductAttributeController implements the CRUD actions for ProductAttribute model.
+ */
+class CategoryAttributeController extends Controller
+{
+    /**
+     * @inheritDoc
+     */
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'delete' => ['POST'],
+                    ],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Lists all ProductAttribute models.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        $searchModel = new CategoryAttributeSearch();
+        $dataProvider = $searchModel->search($this->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single ProductAttribute model.
+     * @param int $id ID
+     * @return string
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new ProductAttribute model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return string|\yii\web\Response
+     */
+    public function actionCreate()
+    {
+        $model = new ProductAttribute();
+
+        if ($this->request->isPost) {
+            if ($model->load($this->request->post()) && $model->save()) {
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
+        } else {
+            $model->loadDefaultValues();
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing ProductAttribute model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param int $id ID
+     * @return string|\yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing ProductAttribute model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param int $id ID
+     * @return \yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the ProductAttribute model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param int $id ID
+     * @return ProductAttribute the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = ProductAttribute::findOne(['id' => $id])) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
---
+++ .history/console/migrations/m240131_011427_create_product_value_table_20240207102645.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%product_value}}`.
+ */
+class m240131_011427_create_product_value_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%product_value}}', [
+            'id' => $this->primaryKey(),
+            'attribute_id' => $this->integer(),
+            'name' => $this->string(),
+            'product_id' => $this->integer(),
+           // 'photo_id' => $this->integer(),     
+            
+        ]);
+        $this->createIndex('index-product_value_attribute_id', 'product_value', 'attribute_id');
+        $this->createIndex('index-product_value_product_id', 'product_value', 'product_id');
+       // $this->createIndex('index-product_value_photo_id', 'product_value', 'photo_id');
+        $this->addForeignKey('fkey-product_value_attribute_id', 'product_value', 'attribute_id', 'product_attribute', 'id', 'CASCADE', 'CASCADE');        
+        $this->addForeignKey('fkey-product_value_product_id', 'product_value', 'product_id', 'product', 'id', 'CASCADE', 'CASCADE');
+       // $this->addForeignKey('fkey-product_value_photo_id', 'product_value', 'photo_id', 'photo', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%product_value}}');
+    }
+}
