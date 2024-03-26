e6121b3605d3d0204f00b2bda1898c437dd84c21
Bahriddin <bahriddin.developer96@gmail.com>
2024-03-26 09:38:28 +0500

    (product view)

---
+++ .history/common/models/Cart_20240326091532.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace common\models;
+use common\models\Product;
+use yii\base\Model;
+
+/*
+ * Array(
+ *      ['cart'] => [
+ *          [2] => [
+ *              'title' => 'TITLE',
+ *              'price' => 10,
+ *              'qty' => 2
+ *          ],
+ *      ],
+ *      'cart.qty' => 2,
+ *      'cart.sum' => 20,
+ * )
+ *
+ * */
+
+class Cart extends Model
+{
+
+    public function addToCart($product, $qty = 1)
+    {
+        //echo 'Worked';
+        $qty = ($qty == '-1') ? -1 : 1;
+        if(isset($_SESSION['cart'][$product->id])){
+            $_SESSION['cart'][$product->id]['qty'] += $qty;
+        }else{
+            $_SESSION['cart'][$product->id] = [
+                'name' => $product->name,
+                'price' => $product->price,
+                'qty' => $qty,
+                'photo' => $product->photos,
+            ];
+        }
+        $_SESSION['cart.qty'] = isset($_SESSION['cart.qty']) ? $_SESSION['cart.qty'] + $qty : $qty;
+        $_SESSION['cart.sum'] = isset($_SESSION['cart.sum']) ? $_SESSION['cart.sum'] + $qty * $product->price : $qty * $product->price;
+        if($_SESSION['cart'][$product->id]['qty'] == 0){
+            unset($_SESSION['cart'][$product->id]);
+        }
+    }
+
+    public function recalc($id)
+    {
+        if(!isset($_SESSION['cart'][$id])){
+            return false;
+        }
+        $qtyMinus = $_SESSION['cart'][$id]['qty'];
+        $sumMinus = $_SESSION['cart'][$id]['qty'] * $_SESSION['cart'][$id]['price'];
+        $_SESSION['cart.qty'] -= $qtyMinus;
+        $_SESSION['cart.sum'] -= $sumMinus;
+        unset($_SESSION['cart'][$id]);
+    }
+
+}
