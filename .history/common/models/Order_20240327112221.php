e028e67509b8f5d6630e89a607d353f91e591308
Bahriddin <bahriddin.developer96@gmail.com>
2024-03-27 12:12:55 +0500

    (order working)

---
+++ .history/common/models/Order_20240327112221.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace common\models;
+use common\models\OrderItems;
+use yii\behaviors\TimestampBehavior;
+use Yii;
+
+/**
+ * This is the model class for table "order".
+ *
+ * @property int $id
+ * @property int $created_at
+ * @property int $updated_at
+ * @property int $qty
+ * @property float|null $sum
+ * @property int $status
+ * @property string $name
+ * @property string|null $email
+ * @property string $phone
+ * @property string $address
+ *
+ * @property OrderItems[] $orderItems
+ */
+class Order extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'order';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'phone', 'address'], 'required'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['qty', 'status'], 'integer'],
+            [['sum'], 'number'],
+            [['name', 'email', 'phone', 'address'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'qty' => 'Qty',
+            'sum' => 'Sum',
+            'status' => 'Status',
+            'name' => 'Name',
+            'email' => 'Email',
+            'phone' => 'Phone',
+            'address' => 'Address',
+        ];
+    }
+
+    /**
+     * Gets query for [[OrderItems]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrderItems()
+    {
+        return $this->hasMany(OrderItems::class, ['order_id' => 'id']);
+    }
+}
---
+++ .history/console/migrations/m240327_005713_create_order_items_table_20240327110802.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%order_items}}`.
+ */
+class m240327_005713_create_order_items_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%order_items}}', [
+            'id' => $this->primaryKey(),
+            'order_id' => $this->integer(),
+            'product_id' => $this->integer(),
+            'name' => $this->string()->notNull(),
+            'price' => $this->float(),
+            'qty_item' => $this->integer(11)->notNull(),
+            'sum_item' => $this->float(),
+
+        ]);
+        $this->createIndex('index-order_items_order_id', 'order_items', 'order_id');
+        $this->createIndex('index-order_items_product_id', 'order_items', 'product_id');       
+        $this->addForeignKey('fkey-order_items_order_id', 'order_items', 'order_id', 'order', 'id', 'CASCADE', 'CASCADE');        
+        $this->addForeignKey('fkey-order_items_product_id', 'order_items', 'product_id', 'product', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%order_items}}');
+    }
+}
