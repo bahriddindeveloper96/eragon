03a24109356e0acca3b414e8b5bc1acf7e2377d2
Bahriddin <bahriddin.developer96@gmail.com>
2024-05-26 16:10:01 +0500

    (26.05.2024s)

--- console/migrations/m240131_011427_create_product_value_table.php
+++ console/migrations/m240131_011427_create_product_value_table.php
@@ -23,7 +23,7 @@ class m240131_011427_create_product_value_table extends Migration
         $this->createIndex('index-product_value_attribute_id', 'product_value', 'attribute_id');
         $this->createIndex('index-product_value_product_items_id', 'product_value', 'product_items_id');       
         $this->addForeignKey('fkey-product_value_attribute_id', 'product_value', 'attribute_id', 'category_attribute', 'id', 'CASCADE', 'CASCADE');        
-        $this->addForeignKey('fkey-product_value_product_id', 'product_value', 'product_items_id', 'product', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('fkey-product_value_product_id', 'product_value', 'product_items_id', 'product_items', 'id', 'CASCADE', 'CASCADE');
        // $this->addForeignKey('fkey-product_value_photo_id', 'product_value', 'photo_id', 'photo', 'id', 'CASCADE', 'CASCADE');
     }
 
